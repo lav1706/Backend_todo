0eac40a3c3f2ad0376aa3c5f2e7059c29b35292e
lav1706 <134134211+lav1706@users.noreply.github.com>
2024-03-06 23:39:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,12 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', [process.env.FRONTEND_URL]);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
